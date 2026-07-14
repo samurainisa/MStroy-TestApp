080ea533e0a60fa8298dc6a850834e00d5540d79
Садовиков Никита <aressamurai9@gmail.com>
2026-07-14 15:19:23 +0700

    feat: правка стилизации отступов и шевронов

--- src/components/table/AgGrid.vue
+++ src/components/table/AgGrid.vue
@@ -59,16 +59,67 @@ const defaultColDef: ColDef<TableRow> = {
 </script>
 
 <template>
-  <ag-grid-vue
-    style="height: 100%"
-    :row-data="tableRows"
-    :column-defs="columnDefs"
-    :get-row-id="getRowId"
-    :default-col-def="defaultColDef"
-    :tree-data="true"
-    :suppress-group-cell-sticky="true"
-    :animate-rows="true"
-    tree-data-parent-id-field="__parentRowId"
-    tree-data-display-type="custom"
-  />
+  <div class="table-panel">
+    <div class="table-panel__mode-bar">
+      Режим: просмотр
+    </div>
+
+    <ag-grid-vue
+      class="table-panel__grid"
+      :row-data="tableRows"
+      :column-defs="columnDefs"
+      :get-row-id="getRowId"
+      :default-col-def="defaultColDef"
+      :tree-data="true"
+      :suppress-group-cell-sticky="true"
+      :animate-rows="true"
+      tree-data-parent-id-field="__parentRowId"
+      tree-data-display-type="custom"
+    />
+  </div>
 </template>
+
+<style scoped>
+.table-panel {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: #eeeeee;
+}
+
+.table-panel__mode-bar {
+  padding: 8px 12px;
+  color: #1a73e8;
+  font-size: 14px;
+}
+
+.table-panel__grid {
+  height: 100vh;
+  min-height: 0;
+}
+
+.table-panel__grid :deep(.ag-cell-wrapper) {
+  position: relative;
+  padding-left: calc(
+    20px +
+    var(--ag-row-group-indent-size, 10px) * var(--ag-indentation-level) * 0.5
+  ) !important;
+}
+
+
+.table-panel__grid :deep(.ag-group-expanded),
+.table-panel__grid :deep(.ag-group-contracted) {
+  position: absolute;
+  left: 4px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.table-panel__grid :deep(.ag-group-value) {
+  margin-left: 30px;
+}
+
+.table-panel__grid :deep(.ag-row-group-leaf-indent) {
+  margin-left: 0 !important;
+}
+</style>
